c9a54386150765eb464172f241dd6c51fea8bd74
ayyleesh <xxserpentxbizzarexx@yahoo.co.id>
2015-12-09 17:47:42 +0700

    created landing & dashboard UI

--- Noteit/App_Start/BundleConfig.cs
+++ Noteit/App_Start/BundleConfig.cs
@@ -25,7 +25,9 @@ namespace Noteit
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/agency.css",
+                      "~/Content/bootstrap.min.css"));
         }
     }
 }
